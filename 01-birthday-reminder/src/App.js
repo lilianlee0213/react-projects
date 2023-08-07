909d8dfbf5971047c58a6528367ed3ff3d0b0e41
Lilian Lee <lilianlee0213@gmail.com>
2023-08-07 19:18:38 -0400

    add elements and className

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+	return (
+		<main>
+			<section className="container">
+				<h3>{data.length} birthdays Today</h3>
+				<section>{/* List */}</section>
+				<button type="button">clear all</button>
+			</section>
+		</main>
+	);
 }
 
 export default App;
