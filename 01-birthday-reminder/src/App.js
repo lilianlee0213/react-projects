860af7ea025b6ca2166165ebb172845387bb0d2c
Lilian Lee <lilianlee0213@gmail.com>
2023-08-07 19:28:01 -0400

    button onclick clear data

--- 01-birthday-reminder/src/App.js
+++ 01-birthday-reminder/src/App.js
@@ -2,14 +2,18 @@ import React, {useState} from 'react';
 import data from './data';
 import List from './List';
 function App() {
+	const [isCleared, setIsCleared] = useState(false);
+	const clearAll = () => {
+		setIsCleared(true);
+	};
 	return (
 		<main>
 			<section className="container">
-				<h3>{data.length} birthdays Today</h3>
-				<section>
-					<List />
-				</section>
-				<button type="button">clear all</button>
+				<h3>{isCleared ? '0' : data.length} birthdays Today</h3>
+				<section>{!isCleared && <List />}</section>
+				<button type="button" onClick={clearAll}>
+					{isCleared ? 'No more upcoming birthdays' : 'clear all'}
+				</button>
 			</section>
 		</main>
 	);
