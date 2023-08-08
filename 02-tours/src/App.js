d748093956441d12441653415071a206411133d7
Lilian Lee <lilianlee0213@gmail.com>
2023-08-08 17:23:53 -0400

    refresh with getTours and show loading

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -6,29 +6,48 @@ import Tours from './Tours';
 const url = 'https://course-api.com/react-tours-project';
 function App() {
 	const [tours, setTours] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
 	const removeTour = (id) => {
 		const remainingTours = tours.filter((tour) => {
 			return tour.id !== id;
 		});
 		setTours(remainingTours);
 	};
+
 	const getTours = async () => {
+		//when refresh show loading again
+		setIsLoading(true);
 		await fetch(`${url}`)
 			.then((res) => res.json())
 			.then((data) => {
 				setTours(data);
+				setIsLoading(false);
 			});
 	};
+
 	useEffect(() => {
 		getTours();
 	}, []);
-	return tours === [] ? (
+
+	return isLoading ? (
 		<Loading />
+	) : tours.length === 0 ? (
+		<main>
+			<section>
+				<div className="title">
+					<h2>No Tours Left</h2>
+					<button className="btn" onClick={() => getTours()}>
+						Refresh
+					</button>
+				</div>
+			</section>
+		</main>
 	) : (
 		<main>
 			<section>
 				<div className="title">
-					<h2>our tours</h2>
+					<h2>Our Tours</h2>
 					<div className="underline"></div>
 				</div>
 				<Tours tours={tours} removeTour={removeTour} />
