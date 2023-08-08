df8bb51b8b3553a3af3285a2ebb86e1a8408b1aa
Lilian Lee <lilianlee0213@gmail.com>
2023-08-08 18:08:51 -0400

    getRandom person

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -3,20 +3,29 @@ import people from './data';
 import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa';
 
 const Review = () => {
+	const [person, setPerson] = useState(people[0]);
+	const getRandom = () => {
+		const randomIndex = Math.floor(Math.random() * people.length);
+		//need to figure out when random index is the same with the next random index
+		setPerson(people[randomIndex]);
+	};
+	const {id, name, job, image, text} = person;
 	return (
 		<article className="review">
 			<div className="img-container">
-				<img src="" alt="" className="person-img" />
+				<img src={image} alt={name} className="person-img" />
 				<span className="quote-icon">{FaQuoteRight()}</span>
 			</div>
-			<h4 className="author"></h4>
-			<p className="job"></p>
-			<p className="info"></p>
+			<h4 className="author">{name}</h4>
+			<p className="job">{job}</p>
+			<p className="info">{text}</p>
 			<div className="btn-container">
 				<button className="prev-btn">{FaChevronLeft()}</button>
 				<button className="next-btn">{FaChevronRight()}</button>
 			</div>
-			<button className="btn random-btn">Suprise me</button>
+			<button className="btn random-btn" onClick={getRandom}>
+				Suprise me
+			</button>
 		</article>
 	);
 };
