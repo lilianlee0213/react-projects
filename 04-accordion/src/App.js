9c9d07027bb8bb0f47e1f89f1fd961d264467441
Lilian Lee <lilianlee0213@gmail.com>
2023-08-08 19:19:19 -0400

    pass props into Question and open question

--- 04-accordion/src/App.js
+++ 04-accordion/src/App.js
@@ -1,11 +1,14 @@
 import React, {useState} from 'react';
 import data from './data';
-import SingleQuestion from './Question';
+import Question from './Question';
 function App() {
 	return (
 		<main>
 			<section className="container">
 				<h1>Questions</h1>
+				{data.map((question) => {
+					return <Question key={question.id} {...question} />;
+				})}
 			</section>
 		</main>
 	);
--- 04-accordion/src/Question.js
+++ 04-accordion/src/Question.js
@@ -1,7 +1,18 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, {useState} from 'react';
+import {AiOutlineMinus, AiOutlinePlus} from 'react-icons/ai';
+const Question = ({title, info}) => {
+	const [isOpened, setIsOpened] = useState(false);
+	return (
+		<article className="question">
+			<header>
+				<h5>{title}</h5>
+				<button className="btn" onClick={() => setIsOpened(!isOpened)}>
+					{isOpened ? AiOutlineMinus() : AiOutlinePlus()}
+				</button>
+			</header>
+			{isOpened && <p>{info}</p>}
+		</article>
+	);
 };
 
 export default Question;
