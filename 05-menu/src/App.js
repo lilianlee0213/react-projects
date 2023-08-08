4e971079e424199d707c1c6a86ad744b1f3db661
Lilian Lee <lilianlee0213@gmail.com>
2023-08-08 19:42:03 -0400

    add elements and pass props

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -1,10 +1,39 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
 function App() {
-  return <h2>menu project setup</h2>;
+	const [menuItems, setMenuItems] = useState(items);
+	return (
+		<main>
+			<section className="menu">
+				<div className="title">
+					<h2>our menu</h2>
+					<div className="underline"></div>
+				</div>
+				<div className="btn-container">
+					<button type="button" className="filter-btn">
+						all
+					</button>
+					<button type="button" className="filter-btn">
+						breakfast
+					</button>
+					<button type="button" className="filter-btn">
+						lunch
+					</button>
+					<button type="button" className="filter-btn">
+						shakes
+					</button>
+				</div>
+				<div className="section-center">
+					{menuItems.map((menu) => {
+						return <Menu key={menu.id} {...menu} />;
+					})}
+				</div>
+			</section>
+		</main>
+	);
 }
 
 export default App;
