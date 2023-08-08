cab7f29fd587b6fb9f37ea2bab1d3443e7cfa7f3
Lilian Lee <lilianlee0213@gmail.com>
2023-08-08 15:36:43 -0400

    jsx elements

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -1,11 +1,39 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, {useState, useEffect} from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://course-api.com/react-tours-project';
 function App() {
-  return <h2>Tours Project Setup</h2>
+	return (
+		<main>
+			<section>
+				<div className="title">
+					<h2>our tours</h2>
+					<div className="underline"></div>
+				</div>
+				<div className="tours">
+					<article className="single-tour">
+						<img src="" alt="" className="img" />
+						<span className="tour-price">$1,995</span>
+						<div className="tour-info">
+							<h5>Best of Prais in 7 Days Tour</h5>
+							<p>
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum
+								impedit veniam possimus rem velit suscipit repellendus amet,
+								maiores ad, quidem officiis provident rerum ex natus expedita
+								neque vitae perspiciatis! Ea!
+								<button className="info-btn">read more</button>
+							</p>
+							<button className="delete-btn btn-block btn">
+								not interested
+							</button>
+						</div>
+					</article>
+				</div>
+			</section>
+		</main>
+	);
 }
 
-export default App
+export default App;
