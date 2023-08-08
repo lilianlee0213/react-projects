3d06a0cf0320f49edca9227fe22b95d71fe80ed8
Lilian Lee <lilianlee0213@gmail.com>
2023-08-08 15:43:03 -0400

    tour and tours component render

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -12,25 +12,7 @@ function App() {
 					<h2>our tours</h2>
 					<div className="underline"></div>
 				</div>
-				<div className="tours">
-					<article className="single-tour">
-						<img src="" alt="" className="img" />
-						<span className="tour-price">$1,995</span>
-						<div className="tour-info">
-							<h5>Best of Prais in 7 Days Tour</h5>
-							<p>
-								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum
-								impedit veniam possimus rem velit suscipit repellendus amet,
-								maiores ad, quidem officiis provident rerum ex natus expedita
-								neque vitae perspiciatis! Ea!
-								<button className="info-btn">read more</button>
-							</p>
-							<button className="delete-btn btn-block btn">
-								not interested
-							</button>
-						</div>
-					</article>
-				</div>
+				<Tours />
 			</section>
 		</main>
 	);
--- 02-tours/src/Tour.js
+++ 02-tours/src/Tour.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 const Tour = () => {
-  return <h2>tour component</h2>;
+	return (
+		<article className="single-tour">
+			<img src="" alt="" className="img" />
+			<span className="tour-price">$1,995</span>
+			<div className="tour-info">
+				<h5>Best of Prais in 7 Days Tour</h5>
+				<p>
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum impedit
+					veniam possimus rem velit suscipit repellendus amet, maiores ad,
+					quidem officiis provident rerum ex natus expedita neque vitae
+					perspiciatis! Ea!
+					<button className="info-btn">read more</button>
+				</p>
+				<button className="delete-btn btn-block btn">not interested</button>
+			</div>
+		</article>
+	);
 };
 
 export default Tour;
