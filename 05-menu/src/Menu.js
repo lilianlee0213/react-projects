efafc6eb3f890982d420aeb9351ab57a2cb20ad5
Lilian Lee <lilianlee0213@gmail.com>
2023-08-08 19:45:52 -0400

    add menu elements

--- 05-menu/src/Menu.js
+++ 05-menu/src/Menu.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = ({id, title, category, price, img, desc}) => {
+	return (
+		<article className="menu-item">
+			<img src={img} alt={title} className="photo" />
+			<div className="item-info">
+				<header>
+					<h5>{title}</h5>
+					<span className="item-price">{price}</span>
+				</header>
+				<p className="item-text">{desc}</p>
+			</div>
+		</article>
+	);
 };
 
 export default Menu;
