2e93ec396141b799567e436030ac324240d13102
Lilian Lee <lilianlee0213@gmail.com>
2023-08-08 18:32:01 -0400

    handle next prev and random buttons

--- 03-reviews/src/Review.js
+++ 03-reviews/src/Review.js
@@ -3,13 +3,28 @@ import people from './data';
 import {FaChevronLeft, FaChevronRight, FaQuoteRight} from 'react-icons/fa';
 
 const Review = () => {
-	const [person, setPerson] = useState(people[0]);
+	const [index, setIndex] = useState(0);
 	const getRandom = () => {
 		const randomIndex = Math.floor(Math.random() * people.length);
 		//need to figure out when random index is the same with the next random index
-		setPerson(people[randomIndex]);
+		setIndex(randomIndex);
 	};
-	const {id, name, job, image, text} = person;
+	const onClickPrev = () => {
+		if (index <= 0) {
+			setIndex(people.length - 1);
+		} else {
+			setIndex((prev) => prev - 1);
+		}
+	};
+	const onClickNext = () => {
+		if (index >= people.length - 1) {
+			setIndex(0);
+		} else {
+			setIndex((prev) => prev + 1);
+		}
+	};
+
+	const {name, job, image, text} = people[index];
 	return (
 		<article className="review">
 			<div className="img-container">
@@ -20,8 +35,12 @@ const Review = () => {
 			<p className="job">{job}</p>
 			<p className="info">{text}</p>
 			<div className="btn-container">
-				<button className="prev-btn">{FaChevronLeft()}</button>
-				<button className="next-btn">{FaChevronRight()}</button>
+				<button className="prev-btn" onClick={onClickPrev}>
+					{FaChevronLeft()}
+				</button>
+				<button className="next-btn" onClick={onClickNext}>
+					{FaChevronRight()}
+				</button>
 			</div>
 			<button className="btn random-btn" onClick={getRandom}>
 				Suprise me
