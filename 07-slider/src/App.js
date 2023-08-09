61c10a0353224d6279aec8d801e9ba86bffef825
Lilian Lee <lilianlee0213@gmail.com>
2023-08-09 16:09:07 -0400

    render data

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -3,22 +3,24 @@ import {FiChevronRight, FiChevronLeft} from 'react-icons/fi';
 import {FaQuoteRight} from 'react-icons/fa';
 import data from './data';
 function App() {
+	const [people, setPeople] = useState(data);
+	console.log(people);
 	return (
 		<section className="section-center">
-			<article>
-				<img src="" alt="" className="person-name" />
-				<h5 className="name">maria</h5>
-				<p className="title">hi</p>
-				<p className="text">
-					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et excepturi
-					voluptatum ut nihil ex itaque fuga reiciendis aut! Ipsam accusantium
-					commodi consequatur hic odio? Nobis iste repellendus temporibus vel
-					asperiores?
-				</p>
-				<FaQuoteRight className="icon" />
-				<button className="prev">{FiChevronLeft()}</button>
-				<button className="next">{FiChevronRight()}</button>
-			</article>
+			{people.map((person) => {
+				const {id, name, image, quote, title} = person;
+				return (
+					<article key={id}>
+						<img src={image} alt={name} className="person-img" />
+						<h5 className="name">{name}</h5>
+						<p className="title">{title}</p>
+						<p className="text">{quote}</p>
+						<FaQuoteRight className="icon" />
+						<button className="prev">{FiChevronLeft()}</button>
+						<button className="next">{FiChevronRight()}</button>
+					</article>
+				);
+			})}
 		</section>
 	);
 }
