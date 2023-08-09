dc34a2f48fe92ff5539eb67bf33c1f21735ee44b
Lilian Lee <lilianlee0213@gmail.com>
2023-08-09 11:32:10 -0400

    html elements

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -1,10 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import React, {useState, useEffect} from 'react';
+import {FaAngleDoubleRight} from 'react-icons/fa';
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = 'https://course-api.com/react-tabs-project';
 function App() {
-  return <h2>tabs project setup</h2>
+	return (
+		<section className="job-center">
+			<div className="btn-container">
+				<button className="job-btn active-btn">Tommy</button>
+				<button className="job-btn">BigDrop</button>
+				<button className="job-btn">Cuker</button>
+			</div>
+			<article className="job-info">
+				<h3>Full Stack web developer</h3>
+				<span className="job-company">tommy</span>
+				<p className="job-date">Descember</p>
+				<div>
+					<div className="job-desc">
+						Lorem ipsum dolor sit amet consectetur a
+					</div>
+					<div className="job-desc">
+						Lorem ipsum dolor sit amet consectetur a
+					</div>
+					<div className="job-desc">
+						Lorem ipsum dolor sit amet consectetur a
+					</div>
+				</div>
+			</article>
+		</section>
+	);
 }
 
-export default App
+export default App;
