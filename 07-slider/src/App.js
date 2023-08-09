c7c30aa400b20dc6228ccd1c53a0e79e9ed3134f
Lilian Lee <lilianlee0213@gmail.com>
2023-08-09 16:00:31 -0400

    add html elements

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -1,9 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
+import React, {useState, useEffect} from 'react';
+import {FiChevronRight, FiChevronLeft} from 'react-icons/fi';
+import {FaQuoteRight} from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+	return (
+		<section className="section">
+			<article>
+				<img src="" alt="" className="person-name" />
+				<h5>maria</h5>
+				<p className="title">hi</p>
+				<p className="text">
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et excepturi
+					voluptatum ut nihil ex itaque fuga reiciendis aut! Ipsam accusantium
+					commodi consequatur hic odio? Nobis iste repellendus temporibus vel
+					asperiores?
+				</p>
+				<button className="prev">{FiChevronLeft()}</button>
+				<button className="next">{FiChevronRight()}</button>
+			</article>
+		</section>
+	);
 }
 
 export default App;
