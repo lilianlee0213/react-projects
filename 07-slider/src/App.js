c252dd5b06a69ae7255f496415afe931e981ba2e
Lilian Lee <lilianlee0213@gmail.com>
2023-08-09 16:34:38 -0400

    slide show with setTimeout

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -4,13 +4,31 @@ import {FaQuoteRight} from 'react-icons/fa';
 import data from './data';
 function App() {
 	const [people, setPeople] = useState(data);
-	console.log(people);
+	const [value, setValue] = useState(0);
+	console.log(value);
+	useEffect(() => {
+		const interval = setTimeout(() => {
+			setValue((prev) => (prev === people.length - 1 ? 0 : prev + 1));
+		}, 5000);
+		return () => clearInterval(interval);
+	}, [value]);
+	const getClassName = (index) => {
+		if (value === index) {
+			return 'activeSlide';
+		}
+		if (value > index) {
+			return 'lastSlide';
+		}
+		if (value < index) {
+			return 'nextSlide';
+		}
+	};
 	return (
 		<section className="section-center">
-			{people.map((person) => {
+			{people.map((person, index) => {
 				const {id, name, image, quote, title} = person;
 				return (
-					<article key={id}>
+					<article key={id} className={getClassName(index)}>
 						<img src={image} alt={name} className="person-img" />
 						<h5 className="name">{name}</h5>
 						<p className="title">{title}</p>
