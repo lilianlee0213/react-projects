8a45a7cfca18e4dca8ab099aac7cd6c4d6703601
Lilian Lee <lilianlee0213@gmail.com>
2023-08-10 14:07:04 -0400

    set value and and onSubmit

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,12 +1,23 @@
 import React, {useState} from 'react';
 import data from './data';
 function App() {
-	const [lorem, setLorem] = useState(data);
-	console.log(lorem);
+	const [lorems, setLorems] = useState([]);
+	const [count, setCount] = useState(1);
+	//To get the value from the input when changed
+	const getValue = (e) => {
+		const {value} = e.target;
+		setCount(value);
+	};
+	//generate lorem when the button clicked
+	const generateLorem = (e) => {
+		e.preventDefault();
+		const newLorem = data.slice(0, count);
+		setLorems(newLorem);
+	};
 	return (
 		<section className="section-center">
 			<h3>tired of boring lorem ipsum?</h3>
-			<form action="" className="lorem-form">
+			<form className="lorem-form" onSubmit={generateLorem}>
 				<label htmlFor="amount">paragraphs</label>
 				<input
 					type="number"
@@ -15,17 +26,15 @@ function App() {
 					min="1"
 					step="1"
 					max="8"
-					value="1"
+					value={count}
+					onChange={getValue}
 				/>
 				<button className="btn">generate</button>
 			</form>
 			<article className="lorem-text">
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem, quos!
-					Consectetur temporibus ab voluptatum, quis aperiam, deleniti officiis
-					velit harum praesentium non animi accusamus possimus culpa doloremque
-					id, laudantium in.
-				</p>
+				{lorems.map((p, index) => (
+					<p key={index}>{p}</p>
+				))}
 			</article>
 		</section>
 	);
