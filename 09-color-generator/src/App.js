90c50940197da42c216014d0b29ee96f0fc95b40
Lilian Lee <lilianlee0213@gmail.com>
2023-08-10 14:38:01 -0400

    html element and styles

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -1,10 +1,29 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, {useState} from 'react';
+import SingleColor from './SingleColor';
 
-import Values from 'values.js'
+import Values from 'values.js';
 
 function App() {
-  return <h2>color generator project</h2>
+	return (
+		<main>
+			<section className="container">
+				<h3>color genrator</h3>
+				<form action="" className="color-form">
+					<input type="color" />
+					<input type="text" placeholder="#f15025" />
+					<button className="btn" type="submit">
+						submit
+					</button>
+				</form>
+			</section>
+			<section className="colors">
+				<article className="color false">
+					<p className="percent-value"></p>
+					<p className="color-value"></p>
+				</article>
+			</section>
+		</main>
+	);
 }
 
-export default App
+export default App;
