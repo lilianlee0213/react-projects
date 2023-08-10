fbd2ad71ff48d8ecf9e3c925cde2a4c163b95c27
Lilian Lee <lilianlee0213@gmail.com>
2023-08-10 15:32:22 -0400

    copy clipboard hexValue

--- 09-color-generator/src/SingleColor.js
+++ 09-color-generator/src/SingleColor.js
@@ -2,12 +2,27 @@ import React, {useState, useEffect} from 'react';
 import rgbToHex from './utils';
 
 const SingleColor = ({rgb, weight}) => {
+	const [alert, setAlert] = useState(false);
+	useEffect(() => {
+		const timeOut = setTimeout(() => {
+			setAlert(false);
+		}, 3000);
+		return () => clearInterval(timeOut);
+	}, [alert]);
 	const bgc = rgb.join(',');
 	const hexValue = rgbToHex(...rgb);
+	const handleCopy = () => {
+		setAlert(true);
+		navigator.clipboard.writeText(hexValue);
+	};
 	return (
-		<article className="color" style={{backgroundColor: `rgb(${bgc})`}}>
+		<article
+			className="color"
+			style={{backgroundColor: `rgb(${bgc})`}}
+			onClick={handleCopy}>
 			<p className="percent-value">{weight}%</p>
 			<p className="color-value">{hexValue}</p>
+			{alert && <p className="alert">copied to clipboard</p>}
 		</article>
 	);
 };
