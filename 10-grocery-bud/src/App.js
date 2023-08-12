e5ed9dc22bae58a8331e79720cd934624086f354
Lilian Lee <lilianlee0213@gmail.com>
2023-08-11 21:36:48 -0400

    html and css for grocery container

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -1,9 +1,31 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, {useState, useEffect} from 'react';
+import List from './List';
+import Alert from './Alert';
 
 function App() {
-  return <h2>grocery bud setup</h2>
+	return (
+		<section className="section-center">
+			<form action="" className="grocery-form">
+				<h3>grocery bud</h3>
+				<div className="form-control">
+					<input type="text" className="grocery" placeholder="Add item" />
+					<button type="submit" className="submit-btn">
+						Submit
+					</button>
+				</div>
+			</form>
+			<div className="grocery-container">
+				<div className="grocery-item">
+					<input type="checkbox" className="check-box" />
+					<p className="title">egg</p>
+					<div className="button-container">
+						<button className="edit-btn">Edit</button>
+						<button className="delete-btn">Delete</button>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
 
-export default App
+export default App;
