209d7fd481b933ae2cdafa03821d83918af2a525
Lilian Lee <lilianlee0213@gmail.com>
2023-08-14 14:09:30 -0400

    toggle links container

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -4,15 +4,19 @@ import {links, social} from './data';
 import logo from './logo.svg';
 
 const Navbar = () => {
+	const [isNavShown, setIsNavShown] = useState(false);
+	const toggleNav = () => {
+		setIsNavShown((prev) => !prev);
+	};
 	return (
 		<div className="nav-center">
 			<div className="nav-header">
 				<img src={logo} className="logo" alt="logo" />
-				<button className="nav-toggle">
+				<button className="nav-toggle" onClick={toggleNav}>
 					<FaBars />
 				</button>
 			</div>
-			<div className="links-container">
+			<div className={`links-container ${isNavShown ? 'show-container' : ''}`}>
 				<ul className="links">
 					{links.map((link) => (
 						<li>
