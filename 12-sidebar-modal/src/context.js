3e4037068be9f7988b11f6b793e91ba24de71a14
Lilian Lee <lilianlee0213@gmail.com>
2023-08-14 17:28:53 -0400

    create gloablcontext

--- 12-sidebar-modal/src/context.js
+++ 12-sidebar-modal/src/context.js
@@ -1,1 +1,24 @@
 import React, {useState, useContext} from 'react';
+
+const AppContext = React.createContext();
+const AppProvider = ({children}) => {
+	const [isSidebarOpen, setIsSideBarOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	const handleSidebar = () => {
+		setIsSideBarOpen((prev) => !prev);
+	};
+	const handleModal = () => {
+		setIsModalOpen((prev) => !prev);
+	};
+	return (
+		<AppContext.Provider
+			value={{isSidebarOpen, isModalOpen, handleSidebar, handleModal}}>
+			{children}
+		</AppContext.Provider>
+	);
+};
+
+export const useGloabalContext = () => {
+	return useContext(AppContext);
+};
