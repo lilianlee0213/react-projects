908ec039d8abcb7d86f5cfb7c0893d46ce3dc6c6
Lilian Lee <lilianlee0213@gmail.com>
2023-08-14 17:16:15 -0400

    sidebar html

--- 12-sidebar-modal/src/Sidebar.js
+++ 12-sidebar-modal/src/Sidebar.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import logo from './logo.svg'
-import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
+import React from 'react';
+import logo from './logo.svg';
+import {FaTimes} from 'react-icons/fa';
+import {social, links} from './data';
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
-}
+	return (
+		<aside className="sidebar">
+			<div className="sidebar-header">
+				<img src={logo} alt="logo" className="logo" />
+				<button className="close-btn">
+					<FaTimes />
+				</button>
+			</div>
+			<ul className="links">
+				{links.map((link) => (
+					<li>
+						<a href={link.url}>
+							{link.icon}
+							{link.text}
+						</a>
+					</li>
+				))}
+			</ul>
+			<ul className="social-icons">
+				{social.map((icon) => (
+					<li>
+						<a href={icon.url}>{icon.icon}</a>
+					</li>
+				))}
+			</ul>
+		</aside>
+	);
+};
 
-export default Sidebar
+export default Sidebar;
