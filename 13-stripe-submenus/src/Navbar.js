8a8d27c76a481aa1788955db70c8d8cbd7e34f33
Lilian Lee <lilianlee0213@gmail.com>
2023-08-15 16:58:22 -0400

    navbar toggle

--- 13-stripe-submenus/src/Navbar.js
+++ 13-stripe-submenus/src/Navbar.js
@@ -4,20 +4,43 @@ import {FaBars} from 'react-icons/fa';
 import {useGlobalContext} from './context';
 
 const Navbar = () => {
-	const {isSidebarOpen, openSidebar} = useGlobalContext();
-	console.log(openSidebar);
+	const {openSidebar, openSubmenu, closeSubmenu} = useGlobalContext();
+	const displaySubmenu = (e) => {
+		const page = e.target.textContent;
+		const tempBtn = e.target.getBoundingClientRect();
+		const center = (tempBtn.left + tempBtn.right) / 2;
+		const bottom = tempBtn.bottom - 3;
+		openSubmenu(page, {center, bottom});
+	};
+	const handleCloseSubmenu = (e) => {
+		if (!e.target.classList.contains('nav-link')) {
+			closeSubmenu();
+		}
+	};
 	return (
-		<nav className="nav">
+		<nav className="nav" onMouseOver={handleCloseSubmenu}>
 			<div className="nav-center">
 				<img src={logo} alt="" />
 				<button className="toggle-btn" onClick={openSidebar}>
 					<FaBars />
 				</button>
-				<div className="nav-links">
-					<button className="nav-link">Product</button>
-					<button className="nav-link">Solutions</button>
-					<button className="nav-link">Resources</button>
-				</div>
+				<ul className="nav-links">
+					<li>
+						<button className="nav-link" onMouseOver={displaySubmenu}>
+							Product
+						</button>
+					</li>
+					<li>
+						<button className="nav-link" onMouseOver={displaySubmenu}>
+							Solutions
+						</button>
+					</li>
+					<li>
+						<button className="nav-link" onMouseOver={displaySubmenu}>
+							Resources
+						</button>
+					</li>
+				</ul>
 			</div>
 		</nav>
 	);
--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -1,11 +1,13 @@
 import React, {useState, useRef, useEffect} from 'react';
+import {useGlobalContext} from './context';
 
 const Submenu = () => {
+	const {isSubmenuOpen, location, coordinates} = useGlobalContext();
 	return (
-		<div className="submenu">
+		<aside className={isSubmenuOpen ? 'submenu show' : 'submenu'}>
 			<h5></h5>
 			<div className="submenu-center"></div>
-		</div>
+		</aside>
 	);
 };
 
