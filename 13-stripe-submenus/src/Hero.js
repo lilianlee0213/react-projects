05c778b5108597bf7591f310d50a3881af44cfd0
Lilian Lee <lilianlee0213@gmail.com>
2023-08-15 15:29:53 -0400

    hero elements and styles

--- 13-stripe-submenus/src/Hero.js
+++ 13-stripe-submenus/src/Hero.js
@@ -1,8 +1,22 @@
-import React from 'react'
-import phoneImg from './images/phone.svg'
+import React from 'react';
+import phoneImg from './images/phone.svg';
 
 const Hero = () => {
-  return <h2>hero component</h2>
-}
+	return (
+		<div className="hero-container">
+			<div className="hero-center">
+				<h1>
+					Manage Any Content
+					<br />
+					Anywhere
+				</h1>
+				<p>
+					Strapi is the leading open-source headless CMS. Ti's 100% JavaScript
+					and fully customizable.
+				</p>
+			</div>
+		</div>
+	);
+};
 
-export default Hero
+export default Hero;
