20e5f8d55bb71d8f0f224d056f86521b9f7de4c4
Lilian Lee <lilianlee0213@gmail.com>
2023-08-15 15:16:46 -0400

    navbar elements

--- 13-stripe-submenus/src/Navbar.js
+++ 13-stripe-submenus/src/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
+import React from 'react';
+import logo from './images/logo.svg';
+import {FaBars} from 'react-icons/fa';
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+	return (
+		<nav className="nav">
+			<div className="nav-center">
+				<img src={logo} alt="" />
+				<button className="toggle-btn">
+					<FaBars />
+				</button>
+				<div className="nav-links">
+					<button className="nav-link">Product</button>
+					<button className="nav-link">Solutions</button>
+					<button className="nav-link">Resources</button>
+				</div>
+			</div>
+		</nav>
+	);
+};
 
-export default Navbar
+export default Navbar;
