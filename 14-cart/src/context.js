818c934f3d6c823399b8610db9e5a7603093322e
Lilian Lee <lilianlee0213@gmail.com>
2023-08-15 19:55:55 -0400

    useReducer and create state and dispatch

--- 14-cart/src/context.js
+++ 14-cart/src/context.js
@@ -1,27 +1,50 @@
-import React, { useState, useContext, useReducer, useEffect } from 'react'
-import cartItems from './data'
-import reducer from './reducer'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-useReducer-cart-project'
-const AppContext = React.createContext()
+import React, {useState, useContext, useReducer, useEffect} from 'react';
+import cartItems from './data';
+const url = 'https://course-api.com/react-useReducer-cart-project';
+const AppContext = React.createContext();
 
-const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems)
-
-  return (
-    <AppContext.Provider
-      value={{
-        cart,
-      }}
-    >
-      {children}
-    </AppContext.Provider>
-  )
-}
+const reducer = (state, action) => {};
+const initialState = {
+	count: 0,
+	cart: cartItems,
+	total: 0,
+	amount: 0,
+};
+const AppProvider = ({children}) => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+	//increase
+	const increase = () => {};
+	//descrease
+	const decrease = () => {};
+	//remove
+	const remove = () => {};
+	//clear
+	const clearCart = () => {};
+	//fetch
+	const fetchCart = async () => {
+		await fetch(`${url}`)
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	};
+	useEffect(() => {
+		fetchCart();
+	}, []);
+	return (
+		<AppContext.Provider
+			value={{
+				...state,
+				increase,
+				decrease,
+				remove,
+				clearCart,
+			}}>
+			{children}
+		</AppContext.Provider>
+	);
+};
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+	return useContext(AppContext);
+};
 
-export { AppContext, AppProvider }
+export {AppContext, AppProvider};
