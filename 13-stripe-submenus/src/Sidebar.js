f7d1e03b0388364869d16a995f0ddb478ac43cae
Lilian Lee <lilianlee0213@gmail.com>
2023-08-15 15:46:04 -0400

    sidebar elements and styles

--- 13-stripe-submenus/src/Sidebar.js
+++ 13-stripe-submenus/src/Sidebar.js
@@ -1,9 +1,35 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-import sublinks from './data'
+import React from 'react';
+import {FaTimes} from 'react-icons/fa';
+import sublinks from './data';
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>
-}
+	return (
+		<aside className="sidebar">
+			<div className="sidebar-container ">
+				<button className="close-btn">
+					<FaTimes />
+				</button>
+				<div className="sidebar-links">
+					{sublinks.map((item, index) => (
+						<article key={index}>
+							<h3>{item.page}</h3>
+							<div className="sidebar-sublinks">
+								{item.links.map((link, linkIndex) => {
+									const {url, label, icon} = link;
+									return (
+										<a href={url} key={linkIndex}>
+											{icon}
+											{label}
+										</a>
+									);
+								})}
+							</div>
+						</article>
+					))}
+				</div>
+			</div>
+		</aside>
+	);
+};
 
-export default Sidebar
+export default Sidebar;
